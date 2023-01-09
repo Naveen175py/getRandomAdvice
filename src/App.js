a03b9db53c78588e669137adf41c9042a0d5a76d
code naveen <iitiannaveen175@gmail.com>
2023-01-09 21:22:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState("Do Sleep!");
+  const [adviceID, setAdviceID] = useState("1");
+  const handleAdvice = async () => {
+    const res = await axios.get("https://api.adviceslip.com/advice");
+    handleAdviceId();
+    setAdvice(res.data.slip.advice);
+
+  };
+  const handleAdviceId = async () => {
+    const res = await axios.get("https://api.adviceslip.com/advice");
+    setAdviceID(res.data.slip.id);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <div className="text">
+      <h3>ADVICE #{adviceID}</h3>
+        <div>
+          <p>“{advice}”</p>
+        </div>
+        <hr className="hr1"/>
+        <hr className="hr2"/>
+        <svg width="6" height="15" className="rect1" ><rect width="6" height="15" className="r1"/></svg>
+        <svg width="6" height="15" className="rect2"><rect width="6" height="15" className="r1"/></svg>
+        <button onClick={handleAdvice}><img src="dice.png" alt="dice" /></button>
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
